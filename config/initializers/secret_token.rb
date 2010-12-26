17442f94a492fe2f2ff1ee7d645464c1f1592dec
Donald Ball <donald.ball@gmail.com>
2010-12-26 15:08:35 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charsheet::Application.config.secret_token = 'd6ddb332372a39a14a0437ad5a29a99b1386bd7b9778be06f2d35922e8f7902c9fc6ed4a0ef0cb6613ebbfd9cc6852511d9a89414e411eeb4a0d3302a3e698e1'
