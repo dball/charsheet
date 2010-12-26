2a5462455a75ad80ed85a12646d5974c183b33d4
Donald Ball <donald.ball@gmail.com>
2010-12-26 15:24:52 -0500

    Add mongo_mapper

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'rails3-generators'
+
+gem "bson_ext"
+gem "mongo_mapper"
--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,9 @@ module Charsheet
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.orm :mongo_mapper
+    end
   end
 end
